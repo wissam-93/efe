a212c7505632bac270f8fb7590bcd3063c16423c
wissam-93 <77725477+wissam-93@users.noreply.github.com>
2021-01-20 17:24:32 +0200

    Update project.py

--- project.py
+++ project.py
@@ -8,7 +8,7 @@ Created on Wed Jan 20 11:40:46 2021
 
 """
 
-Columns description
+DATASET  Columns description
 
 anaemia     :Decrease of red blood cells or hemoglobin (boolean)
 
@@ -37,7 +37,7 @@ High_blood_pressure - 0 = No, 1 = Yes
 Smoking - 0 = No, 1 = Yes
 DEATH_EVENT - 0 = No, 1 = Yes
 
-Binary 
+All Binary Value
 """
 
 
@@ -54,62 +54,49 @@ df = pd.read_csv('heart_failure_clinical_records_dataset.csv')
 
 # Checking our Dataset + Basic Operations
 
-"""
-1.
 
 x = pd.DataFrame(df.describe()) # indataframe 
 print(x.describe())
 
-The describe() function computes a summary of statistics 
-only numeric coulumns
-
-"""
+#The describe() function computes a summary of statistics 
+#only numeric coulumns
 
 
 
-"""
-2.
+
 
 print(x.info())
-info() method prints information about a DataFrame 
-including the index dtype and column dtypes, non-null values and memory usage
-"""
+#info() method prints information about a DataFrame 
+#including the index dtype and column dtypes, non-null values and memory usage
 
 
-"""
-3.
+
+
 print(x.head())
-Head()  method return top 5 rows of a data frame
-"""
+#Head()  method return top 5 rows of a data frame
 
 
 
-"""
-4.
+
 print(x.dtypes)
-#our data is only numurical
-dtype Return a subset of the DataFrame’s columns based on the column dtypes.
-"""
+# our data is only numurical
+# dtype Return a subset of the DataFrame’s columns based on the column dtypes.
 
 
-"""
-5.
+
+
 
 #checking our columns list
 Print(list(x.columns))
 
-"""
-
-
 
 
 # Data  Cleaning
-"""
 print(x.isnull().sum())
+# There is no Missing Values no need for replacing/fillna
+# OUR DATA IS CLEAN
 
-#There is no Missing Values
 
-"""
 
 
 
@@ -117,14 +104,17 @@ print(x.isnull().sum())
 
 
 #Check coorelation between each column "variables"
-
-#x = pd.DataFrame(df.corr())
+x = pd.DataFrame(df.corr())
 
 """
+ourtarget is death event
 correlation between Death Event and other features, 
 the corrolation between death_event and sex ,diabetes is weak!!
 
-ourtarget is death event
+There is many way to measure correlation by apllying PEARSON/ SPEARMEN
+
+I PREFERED corr() method to avoid complexity , * might  be  wrong measure 
+
 """
 
 
@@ -135,8 +125,11 @@ df = pd.DataFrame(df.drop('sex', axis=1))
 df = pd.DataFrame(df.drop('diabetes', axis=1))
 
 
+#checking our data afte drp sex/diabetes
+print(df.shape)
 
-#print(df.shape)
+
+#data preproccesing +modeling+split test
 
 
 X=df.drop(['DEATH_EVENT'],axis=1)
@@ -146,7 +139,9 @@ from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2,random_state=5)
 
+
 """
+
 print(X_train.shape)
 print(y_train.shape)
 print(X_test.shape)
@@ -160,12 +155,30 @@ from sklearn.linear_model import LogisticRegression
 
 X=df[['creatinine_phosphokinase','ejection_fraction','high_blood_pressure','platelets','serum_creatinine','serum_sodium','smoking','time']].values
 y=df[['DEATH_EVENT']].values
-#SPLIT
+
+
+
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
 y_train=y_train.ravel()
 y_test=y_test.ravel()
 
+""" 
+error 
 
+E:\anaconda3\lib\site-packages\sklearn\utils\validation.py:72:
+DataConversionWarning: A column-vector y was passed when a 1d array was expected. Please change the shape of y to (n_samples, ), f
+or example using ravel(). return f(**kwargs)
+
+y_train=y_train.ravel()
+y_test=y_test.ravel()
+
+MUST   USE   ravel()  
+
+The ravel() function is used to create a contiguous flattened array. A 1-D array, containing the elements of the input
+
+#https://numpy.org/doc/stable/reference/generated/numpy.ravel.html
+
+"""
 
 
 
@@ -178,5 +191,5 @@ y_pred=model.predict(X_test)
 print(model.score(X_test,y_test))
 
 
-
+#output = 0.8833333333333333
 
